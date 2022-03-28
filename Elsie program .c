9377b63b2ad7bba2938db6d99f76739d5a5506b5
Elsie-kay <101639829+Elsie-kay@users.noreply.github.com>
2022-03-28 17:59:32 +0300

    Add files via upload

---
+++ Elsie program .c
@@ -0,0 +1,13 @@
+//program to write my name
+#include<stdio.h>
+
+int main()
+{
+int Name;
+int Name2;
+int Name3;
+    printf("Elsie\n");
+    printf("Nekesa\n");
+    printf("Chonge\n");
+return '0';
+}
---
+++ area of circle .c
@@ -0,0 +1,12 @@
+//program to find sum of five numbers
+#include<stdio.h>
+
+int main()
+{
+int PI = 3.14;
+int Radius = 14;
+int area;
+area= PI*Radius*Radius;
+printf("area of a circle:%d",area);
+return 0;
+}
